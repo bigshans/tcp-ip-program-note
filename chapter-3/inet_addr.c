5927d5dc194a986a9403475db6eea32cf0aa9e29
bigshans <26884666+bigshans@users.noreply.github.com>
2023-02-13 14:40:01 +0800

    WIP: 阅读第三章

---
+++ chapter-3/inet_addr.c
@@ -0,0 +1,21 @@
+#include "../common.h"
+
+int main(int argc, char **argv)
+{
+    char *addr1 = "1.2.3.4";
+    char *addr2 = "1.2.3.256";
+
+    ulong conv_addr = inet_addr(addr1);
+    if (conv_addr == INADDR_NONE) {
+        printf("Error occured!\n");
+    } else {
+        printf("Network ordered integer addr: %#lx\n", conv_addr);
+    }
+    if (conv_addr == INADDR_NONE) {
+        printf("Error occured!\n");
+    } else {
+        printf("Network ordered integer addr: %#lx\n", conv_addr);
+    }
+    conv_addr = inet_addr(addr2);
+    return 0;
+}
